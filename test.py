f8cee3c06a4bbf2eed324c84ea644a8a682af9b3
kjiang8 <kjiang@ctemc.org>
2015-12-28 01:19:50 -0500

    functions to test if there are stats, to get stats, and to rank

--- test.py
+++ test.py
@@ -6,6 +6,14 @@ import requests
 
 def getStats(ID): #gets team stats
 
+	stats = []
+	ppb = []
+	powers = []
+	teams = []
+	page = 0
+	tree = 0
+	allteams = 0
+
 	hasStats = isStats(ID)
 
 	if hasStats:
@@ -14,29 +22,36 @@ def getStats(ID): #gets team stats
 
 		tree = html.fromstring(page.content)
 
-		stats = []
-		ppb = []
-		powers = []
-
 		allteams =  tree.xpath('//a/text()') #gets everything in <a> tags
 		teams = allteams[15:] #removes header stuff
 		teams = teams[:len(teams)-3] #removes footer stuff
 
+		print(teams)
+
 		stats.append(tree.xpath('//td[@align="RIGHT"]/text()')) #gets all stats
 
-		set = raw_input("what set is this: ")
+		setName = raw_input("what set is this: ")
+		#setName = "IS-148" #laziness is real
 
 		f = open('stats','a') #.txt file with everything
 
-		for i in range(len(teams)):
-			ppb.append(stats[0][17*i+16]) #gets ppb
-			powers.append(stats[0][17*i+7]) #gets powers
+		#16 or 17th each, every 8th is powers
 
-			string = str([teams[i], str(set), float(ppb[i]), int(powers[i])])
+		for i in range(len(teams)):
+	
+			try:
+				ppb.append(stats[0][17*i+16]) #gets ppb
+				powers.append(stats[0][17*i+7]) #gets powers
+			except IndexError:
+				pass
+
+			#print(teams[i], ppb[i], powers[i])
+			string = str(teams[i]) + ", " + str(setName) + ", " + str(ppb[i]) + ", " + str(powers[i])
 			
 			f.write(string+"\n")
-			#print(teams[i], ppb[i], powers[i])
-
+			
+		#print (ppb)
+		#print (powers)
 
 		f.close()
 
@@ -62,5 +77,34 @@ def isStats(ID): #checks to see if tournament has stats uplaoded
 
 	return hasStats
 
+def rank():
 
+	with open('stats')as f:
+		content = f.read().splitlines()
+
+	#content = map(lambda x:x.split(),content)
+
+	#print(len(content))
+	#print(content[0])
+	#print(content[0][2])
+	#print(content)
+
+	content = map(lambda x: x.split(','), content)
+
+	for i in range(len(content)):
+		content[i][2] = float(content[i][2]) #convert string to float
+
+	#print(content)
+	#print(content[0])
+	#print(content[0][2])
+
+	sort = sorted(content, key=getKey, reverse=True)
+
+
+	for i in range(25):
+		print(i+1,sort[i][0],sort[i][1],sort[i][2])
+
+
+def getKey(item):
+	return item[2] #sort by ppb
 
