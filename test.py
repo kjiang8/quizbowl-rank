be80311b0097e86d3400ba33069b0340468a0912
Ray Zeng <raisins2011@gmail.com>
2015-12-27 22:28:33 -0500

    Added Python 2.x compatibility for 3.x

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
 from lxml import html
 import requests
 
@@ -14,8 +17,8 @@ for i in range(11):
 	
 ppb.append(tree.xpath('//td[@align="RIGHT"]/text()'))
 
-print teams
-print ppb
+print(teams)
+print(ppb)
 
 #ppb gets everything with <td align=RIGHT> including all stats
 
