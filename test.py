010a61a56526e07a3dbca1e4db7da03d7d6fc86b
kjiang8 <kjiang@ctemc.org>
2015-12-27 22:18:42 -0500

    teams gets team names, ppg gets everything

---
+++ test.py
@@ -0,0 +1,22 @@
+from lxml import html
+import requests
+
+page = requests.get('http://www.hsquizbowl.org/db/tournaments/3502/stats/combined/')
+
+tree = html.fromstring(page.content)
+
+teams = []
+ppb = []
+
+for i in range(11):
+
+	teams.append(tree.xpath('//a[@href="tournaments/3502/stats/combined/teamdetail/#t'+ str(i) + '"]/text()'))
+	
+ppb.append(tree.xpath('//td[@align="RIGHT"]/text()'))
+
+print teams
+print ppb
+
+#ppb gets everything with <td align=RIGHT> including all stats
+
+
