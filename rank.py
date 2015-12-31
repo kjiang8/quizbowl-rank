edf14c1b25c28ce31731899fb5d19b7ba1f7d618
kjiang8 <kjiang@ctemc.org>
2015-12-31 17:25:13 -0500

    clean up code, remove need for naut, house writes files

--- rank.py
+++ rank.py
@@ -2,19 +2,17 @@
 BACKLOG
 
 automatically get set name (which is in same url as where isStats looks)
-remove need for housewrites file, cut content by those only with that housewrite to make things more efficient
-fix all the manual copy/pasting things
+clean up code
 
 INSTRUCTIONS:
-1. add tournament stats to file: run getStats(ID) where ID = 4-digit tournamet ID in url
-	1a. manually input set name when prompted
-2. look over team names for consistency: run alphabetize()
-2a. manually replace pseudonyms, etc
-3. if housewrite, copy over statlines to housewrites file
-3a. if IS-set, run copyNAQT() to copy naqt statlines to appb
-	warning: this will copy all IS-sets, including the ones already in appb, so delete old ones or fix this
-4. for each housewrite set, run adjust('NAME OF HOUSEWRITE')
-5. once all ppbs are adjusted and transferred to appb file, run makeDict() to show team rankings
+1. 	add tournament stats to file: run getStats(ID) where ID = 4-digit tournamet ID in url
+		1a. if report is not called "all_games" or "combined", manually input report name when promtped
+		1b. manually input set name when prompted
+2. 	look over team names for consistency: run alphabetize()
+		2a. manually replace pseudonyms, etc
+3. 	for each housewrite set, run adjust('NAME OF HOUSEWRITE')
+4. 	once all ppbs are adjusted and transferred to appb file, run makeDict() to show team rankings
+		5a. if no new teams are added, run rankinglist()
 
 '''
 
@@ -26,6 +24,7 @@ import requests
 from scipy import stats
 import numpy as np
 import json
+import matplotlib.pyplot as plt
 
 def getStats(ID): #gets team stats
 
@@ -74,7 +73,6 @@ def getStats(ID): #gets team stats
 
 		allstats.append(tree.xpath('//td[@align="RIGHT"]/text()')) #gets all stats
 
-
 		ppbIndex = headers[0].index("P/B") #gets ppb index
 		#print("ppb: ", ppbIndex)
 
@@ -96,6 +94,12 @@ def getStats(ID): #gets team stats
 		f = open('stats','a') #.txt file with everything
 		g = open('ID','a') #.txt file with tournament IDs
 
+		isset = False #flag to see if IS-set
+
+		if str(setName[:3].strip()) == "IS-":
+			isset = True
+			h = open('appb','a') #open appb to copy over naqt stats
+
 		for i in range(len(teams)):
 
 			try:
@@ -108,16 +112,23 @@ def getStats(ID): #gets team stats
 			print(teams[i], ppb[i], powers[i], games[i])
 			string = str(teams[i]) + ", " + str(setName) + ", " + str(ppb[i]) + ", " + str(powers[i]) + ', ' + str(games[i])
 			
-			f.write(string+"\n")
-		g.write(str(ID)+"\n")
+			f.write(string+"\n") #write stats to file
+
+			if isset:
+				h.write(str(teams[i]) + ", " + str(setName) + ", " + str(ppb[i])+"\n") #writes to appb file
+
+		g.write(str(ID)+"\n") #writes ID to file
 
 		f.close()
+		g.close()
+
+		if isset:
+			h.close()
 
 		print("added stats")
 
 	else:
 		print("no stats")
-
 
 def isStats(ID): #checks to see if tournament has stats uplaoded
 	
@@ -140,7 +151,7 @@ def adjust(housewrite): #adjusts ppb for housewrites
 	#stdev, mean = getNAQT()
 	#print(mean, stdev)
 
-	with open('housewrites') as f:
+	with open('stats') as f:
 		content = f.read().splitlines()
 
 	content = map(lambda x: x.split(','), content)
@@ -170,29 +181,6 @@ def adjust(housewrite): #adjusts ppb for housewrites
 
 	f.close()
 
-def getNAQT(): #gets stdev, mean from naqt stats
-
-	with open('stats')as f:
-		content = f.read().splitlines()
-
-	content = map(lambda x: x.split(','), content)
-
-	a = []
-
-	for i in range(len(content)):
-		if str(content[i][1][:4].strip()) == 'IS-':	
-			a.append(float(content[i][2])) #append float ppb
-
-	#a = sorted(a, reverse=True)
-	#a = a[:100]
-
-	stdev = np.std(a)
-	mean = np.mean(a)
-
-	#print(stdev,mean)
-	return(stdev, mean)
-
-
 def makeDict(): #makes json, writes to dict.txt file, ranks teams
 	oldDict = {}
 	try:
@@ -218,18 +206,19 @@ def makeDict(): #makes json, writes to dict.txt file, ranks teams
 	#print(oldDict)
 	rankinglist(oldDict)
 
-
 def sortingDict(d): #sorts all aPPB for each team high to low
 	#d = dictionary
 	for key in d:
 		d[key] = sorted(d[key].values(), reverse=True)
 	return d
 
-def rankinglist(d): #takes top 3 appb of each team, averages, prints sorted list
+def rankinglist(d=None,x=25): #takes top 3 appb of each team, averages, prints sorted list of top x teams, defaults to 25
 	
-	b = []
+	if d==None:
+		d = json.load(open('dict.txt'))
 
-	sortingDict(d)
+	b = []
+	sortingDict(d) #within each team, appbs are high to low
 	#print(d)
 
 	for key in d:
@@ -243,8 +232,96 @@ def rankinglist(d): #takes top 3 appb of each team, averages, prints sorted list
 	sort = sorted(b, key=lambda x: x[1], reverse=True)
 	#print(sort)
 
-	for i in range(50):
+	for i in range(x):
 		print(i+1,sort[i][0],round(sort[i][1],2))
+
+def alphabetize(): #prints alphabetized list of team names to look over
+	with open('stats') as f:
+		content = f.read().splitlines()
+
+	content = map(lambda x: x.split(','), content)
+	sorted_content = sorted(content)
+	
+	for i in range(len(content)):
+		print(sorted_content[i][0])
+
+def onlyboth():
+	d = json.load(open('naqtdict'))
+	sortingDict(d)
+	for key in d:
+		d[key] = float(d[key][0])
+		#print(key,d[key])
+
+	with open('housewrites')as f:
+		content = f.read().splitlines()
+
+	content = map(lambda x: x.split(','), content)
+
+	oldDict = {}
+
+	for i in range(len(content)):
+		content[i] = content[i][:3]
+		content[i][2] = float(content[i][2]) #append float ppb
+
+		oldDict[content[i][0]] = {content[i][1].strip(): content[i][2]}
+
+	#print(oldDict)
+
+	newDict = {}
+
+	for key in oldDict:
+		try:
+			if d[key]:
+				newDict[key] = d[key]
+		except KeyError:
+			pass
+
+	#print(newDict,len(newDict))
+
+	a = []
+
+	for key in newDict:
+		a.append(float(newDict[key])) #append float ppb
+
+	#a = sorted(a, reverse=True)
+	#a = a[:50]
+	#print(a)
+
+	stdev = np.std(a)
+	mean = np.mean(a)
+
+	#print(stdev,mean)
+	return(stdev, mean)
+
+
+
+
+
+
+
+##--------------old stuff / playing around with things-----------##
+
+def getNAQT(): #gets stdev, mean from all naqt stats
+
+	with open('stats')as f:
+		content = f.read().splitlines()
+
+	content = map(lambda x: x.split(','), content)
+
+	a = []
+
+	for i in range(len(content)):
+		if str(content[i][1][:4].strip()) == 'IS-':	
+			a.append(float(content[i][2])) #append float ppb
+
+	#a = sorted(a, reverse=True)
+	#a = a[:100]
+
+	stdev = np.std(a)
+	mean = np.mean(a)
+
+	#print(stdev,mean)
+	return(stdev, mean)
 
 def copyNAQT(): #copies over IS-sets from stats to appb
 	with open('stats')as f:
@@ -258,16 +335,62 @@ def copyNAQT(): #copies over IS-sets from stats to appb
 		if str(content[i][1][:4].strip()) == 'IS-':	
 			g.write(str(content[i][0]) + ', ' + str(content[i][1]) + ', ' + str(content[i][2]) + '\n' ) 
 
-def alphabetize(): #prints alphabetized list of team names to look over
-	with open('stats') as f:
+def copyNAQT2(): #copies over IS-sets from stats to naqt
+	with open('stats')as f:
 		content = f.read().splitlines()
 
 	content = map(lambda x: x.split(','), content)
-	sorted_content = sorted(content)
-	
-	for i in range(len(content)):
-		print(sorted_content[i][0])
 
+	g = open('naqt','a')	
+
+	for i in range(len(content)):
+		if str(content[i][1][:4].strip()) == 'IS-':	
+			g.write(str(content[i][0]) + ', ' + str(content[i][1]) + ', ' + str(content[i][2]) + '\n' ) 
+
+def onlytop(): #only looks at best IS-set performance
+	d = json.load(open('naqtdict'))
+	sortingDict(d)
+	for key in d:
+		d[key] = float(d[key][0])
+		print(key,d[key])
+
+	a = []
+
+	for key in d:
+		a.append(float(d[key])) #append float ppb
+
+
+	a = sorted(a, reverse=True)
+	a = a[:50]
+	print(a)
+
+	stdev = np.std(a)
+	mean = np.mean(a)
+
+	#print(stdev,mean)
+	return(stdev, mean)
+
+def naqtrank(): #playing around with just naqt is-set ppbs
+	oldDict = {}
+	with open('naqt')as f:
+		content = f.read().splitlines()
+
+	content = map(lambda x: x.split(','), content)
+
+	for i in range(len(content)):
+		content[i][2] = float(content[i][2]) #append float ppb
+		if content[i][0] in oldDict: #already have a team entry
+			oldDict[content[i][0]][content[i][1].strip()] = content[i][2] #adds set name, ppb as entries in dict
+		else: #making new key for team
+			oldDict[content[i][0]] = {content[i][1].strip(): content[i][2]}
+
+	#print(oldDict)
+	#rankinglist(oldDict)
+
+	sort = sorted(content, key=lambda x: x[2], reverse=True)
+	
+	for i in range(50):
+		print(i+1,sort[i][0], sort[i][2])
 
 def morlan(x): #does adjustments the morlan way
 	with open('stats') as f:
@@ -322,71 +445,24 @@ def morlan(x): #does adjustments the morlan way
 	print(np.mean(new))
 
 	i = 0
-	print('team', 'is-set ppb','hft ppb','difference')
+	print('team,', 'is-set ppb,','hft ppb,','difference')
 	for key in isset:
 		print(key, isset[key],harvard[key],difference[i])
 		i=i+1
 
 
-def copyNAQT2(): #copies over IS-sets from stats to naqt
-	with open('stats')as f:
-		content = f.read().splitlines()
+def graph():
 
-	content = map(lambda x: x.split(','), content)
-
-	g = open('naqt','a')	
-
-	for i in range(len(content)):
-		if str(content[i][1][:4].strip()) == 'IS-':	
-			g.write(str(content[i][0]) + ', ' + str(content[i][1]) + ', ' + str(content[i][2]) + '\n' ) 
-
-
-def naqtrank(): #playing around with just naqt is-set ppbs
-	oldDict = {}
+	'''
 	with open('naqt')as f:
 		content = f.read().splitlines()
 
 	content = map(lambda x: x.split(','), content)
 
 	for i in range(len(content)):
-		content[i][2] = float(content[i][2]) #append float ppb
-		if content[i][0] in oldDict: #already have a team entry
-			oldDict[content[i][0]][content[i][1].strip()] = content[i][2] #adds set name, ppb as entries in dict
-		else: #making new key for team
-			oldDict[content[i][0]] = {content[i][1].strip(): content[i][2]}
+		a.append(float(content[i][2])) #append float ppb
+	'''
 
-	#print(oldDict)
-	#rankinglist(oldDict)
-
-	sort = sorted(content, key=lambda x: x[2], reverse=True)
-	
-	for i in range(50):
-		print(i+1,sort[i][0], sort[i][2])
-
-def onlytop():
-	d = json.load(open('naqtdict'))
-	sortingDict(d)
-	for key in d:
-		d[key] = float(d[key][0])
-		print(key,d[key])
-
-	a = []
-
-	for key in d:
-		a.append(float(d[key])) #append float ppb
-
-
-	a = sorted(a, reverse=True)
-	a = a[:50]
-	print(a)
-
-	stdev = np.std(a)
-	mean = np.mean(a)
-
-	#print(stdev,mean)
-	return(stdev, mean)
-
-def onlyboth():
 	d = json.load(open('naqtdict'))
 	sortingDict(d)
 	for key in d:
@@ -417,23 +493,60 @@ def onlyboth():
 		except KeyError:
 			pass
 
-	#print(newDict,len(newDict))
-
 	a = []
 
 	for key in newDict:
-		a.append(float(newDict[key])) #append float ppb
+		a.append(newDict[key])
 
-	#a = sorted(a, reverse=True)
-	#a = a[:50]
 	#print(a)
 
-	stdev = np.std(a)
-	mean = np.mean(a)
+	with open('housewrites')as g:
+		hw = g.read().splitlines()
 
-	#print(stdev,mean)
-	return(stdev, mean)
+	hw = map(lambda x: x.split(','), hw)
 
+	allhw = [[],[],[],[],[]]
+
+	names = ('bask','cali','umd','hft','limit')
+
+	for i in range(len(hw)):
+		
+		if str(hw[i][1].strip()) == 'BASK':
+			allhw[0].append(float(hw[i][2]))
+		elif str(hw[i][1].strip()) == 'CALI':
+			allhw[1].append(float(hw[i][2]))
+		elif str(hw[i][1].strip()) == 'UMD Fall':
+			allhw[2].append(float(hw[i][2]))
+		elif str(hw[i][1].strip()) == 'Harvard':
+			allhw[3].append(float(hw[i][2]))
+		elif str(hw[i][1].strip()) == 'LIMIT':
+			allhw[4].append(float(hw[i][2]))
+
+	#print(allhw)
+
+	#print(bask, cali, umd, hft, limit)
+	for i in range(len(allhw)):
+		plt.subplot(2,3,i+1)
+		n, bins, patches = plt.hist(allhw[i], len(allhw[i]), normed=1,facecolor='b',alpha=0.75)
+		
+		x_axis = np.arange(0,30,1)
+		plt.plot(x_axis,stats.norm.pdf(x_axis,np.mean(allhw[i]),np.std(allhw[i])))
+
+		plt.title(names[i])
+		plt.axis([0,30,0,0.4])
+
+	plt.subplot(2,3,6)
+	n, bins, patches = plt.hist(a, len(a), normed=1, facecolor='b', alpha=0.75)
+	plt.title('IS-set ppb')
+	#plt.ylabel('some arbitrary thing')
+	#plt.title('Distribution of IS-set ppbs')
+	
+	x_axis = np.arange(0,30,0.01)
+	plt.plot(x_axis,stats.norm.pdf(x_axis,np.mean(a),np.std(a)))
+	plt.axis([0,30,0,0.3])
+
+	#plt.plot(a,b,'ro')
+	plt.show()	
 
 
 
